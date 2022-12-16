b8ad61ed8bb8f386c679b8bda4b4f4334082ea1a
kyum-q <109158497+kyum-q@users.noreply.github.com>
2022-12-16 14:00:20 +0900

    workroom asset setting

---
+++ Assets/Script/cshPlayerMove.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cshPlayerMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(transform.forward * 20.0f * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(transform.forward * -20.0f * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(transform.right * 20.0f * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(transform.right * -20.0f * Time.deltaTime);
+        }
+    }
+
+}
